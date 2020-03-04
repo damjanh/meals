5a3e665627c507acd673406b7f37d3d9e4b8e093
damjanh <damjan.hudobreznik@gmail.com>
2020-03-04 10:25:40 +0100

    Show categories screen.

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,21 +6,26 @@ import 'category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 1.5,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals'),
       ),
-      children: <Widget>[
-        ...DUMMY_CATEGORIES
-            .map((category) => CategoryItem(
-                  title: category.title,
-                  color: category.color,
-                ))
-            .toList()
-      ],
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: <Widget>[
+          ...DUMMY_CATEGORIES
+              .map((category) => CategoryItem(
+                    title: category.title,
+                    color: category.color,
+                  ))
+              .toList()
+        ],
+      ),
     );
   }
 }
