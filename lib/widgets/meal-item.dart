d71eeff22236fc989fea3a7a68be9b8bda18010a
damjanh <damjan.hudobreznik@gmail.com>
2020-03-04 14:13:26 +0100

    Display more info on meal items.

--- lib/widgets/meal-item.dart
+++ lib/widgets/meal-item.dart
@@ -18,6 +18,30 @@ class MealItem extends StatelessWidget {
 
   void _onMealSelect() {}
 
+  String get _complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+      case Complexity.Challenging:
+        return 'Challenging';
+      case Complexity.Hard:
+        return 'Hard';
+    }
+    return '';
+  }
+
+  String get _affordabilityText {
+    switch(affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Pricey:
+        return 'Pricey';
+      case Affordability.Luxurious:
+        return 'Luxurious';
+    }
+    return '';
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -39,10 +63,63 @@ class MealItem extends StatelessWidget {
                   ),
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(15),
-                      bottomLeft: Radius.circular(15)),
+                      topRight: Radius.circular(15)),
                 ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 220,
+                    decoration: BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
               ],
-            )
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6),
+                      Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.local_dining),
+                      SizedBox(width: 6),
+                      Text(_complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      SizedBox(width: 6),
+                      Text(_affordabilityText),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
