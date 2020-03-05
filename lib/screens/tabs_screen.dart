1c408b0f4a68459337ba6bfbbdbcb0aab2667091
damjanh <damjan.hudobreznik@gmail.com>
2020-03-05 08:57:56 +0100

    Implement bottom navigation bar.

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -11,45 +11,52 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Widget> _pages = [
+    CategoriesScreen(),
+    FavoritesScreen(),
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _onPageSelect(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            indicatorWeight: 4,
-            indicatorColor: Colors.white,
-            tabs: <Widget>[
-              Tab(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.category),
-                    SizedBox(width: 4),
-                    Text('Categories'),
-                  ],
-                ),
-              ),
-              Tab(
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(Icons.star),
-                      SizedBox(width: 4),
-                      Text('Favorites'),
-                    ]),
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+      ),
+      body: _pages[_selectedPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onPageSelect,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white70,
+        selectedItemColor: Colors.white,
+        currentIndex: _selectedPageIndex,
+        selectedFontSize: 14,
+        unselectedFontSize: 12,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.category,
+            ),
+            title: Text(
+              'Categories',
+            ),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.star,
+            ),
+            title: Text(
+              'Favorites',
+            ),
+          )
+        ],
       ),
     );
   }
